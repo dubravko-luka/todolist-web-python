651e86c6e187679640f623edc5775036a4a4d1d4
Dubravko <dev.richard.npm98@gmail.com>
2024-06-11 22:21:59 +0700

    update

--- auth.py
+++ auth.py
@@ -1,5 +1,6 @@
 from flask import render_template, request, redirect, url_for, session
 import csv
+import bcrypt
 from models import User
 
 def load_users():
@@ -9,6 +10,12 @@ def load_users():
         for row in reader:
             users.append(User(row[0], row[1], row[2]))
     return users
+
+def hash_password(password):
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
+
+def check_password(hashed_password, plain_password):
+    return bcrypt.checkpw(plain_password.encode('utf-8'), hashed_password.encode('utf-8'))
 
 def save_user(user):
     users = load_users()
@@ -42,7 +49,8 @@ def register_route():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        user = User(username, password)
+        hashed_password = hash_password(password)
+        user = User(username, hashed_password + 'hcmute')
         if save_user(user):
             return redirect(url_for('login'))
         else:
@@ -52,16 +60,16 @@ def register_route():
 def login_route():
     if is_logged_in():
         return redirect(url_for('index'))
+    error = None
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
         users = load_users()
-        user = next((user for user in users if user.username == username and user.password == password), None)
-        if user:
+        user = next((user for user in users if user.username == username), None)
+        if user and check_password(user.password.replace('hcmute', ''), password):
             session['username'] = user.username
             session['role'] = user.role
             return redirect(url_for('index'))
         else:
             error = 'Invalid username or password.'
-            return render_template('login.html', error=error)
-    return render_template('login.html', is_logged_in=is_logged_in())
+    return render_template('login.html', error=error, is_logged_in=is_logged_in())
